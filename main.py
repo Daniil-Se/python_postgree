a6c5fc37fa40b7102887c58f21017ea407959cd1
Daniil <you@example.com>
2019-11-29 10:14:54 +0300

    modified post_conn class and open xlsx class

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from psycopg2.extras import DictCursor #чтобы выводился списо
 
 class postgree_connect:
 
+	''' При создании экземпляра прописыватьимя бд для подключения'''
 	''' имя DB прописывать строкой '''
 
 	def __init__(self, db_name):
@@ -13,38 +14,92 @@ class postgree_connect:
                         password = '2378951', host = 'localhost')
 		self.cursor = self.conn.cursor(cursor_factory = DictCursor)
  
-	def insert(self, table_name):
-		self.cursor.execute("INSERT INTO {} VALUES ('ABC', '32213121') ".format(table_name)) 
+	def insert(self, table_name, values):
+		''' Для инсерта прописывать имя таблицы строкой'''
+		''' Values - передавать кортежем '''
+
+		#self.cursor.execute("INSERT INTO {} VALUES ('ABC', '32213121') ".format(table_name)) 
+		self.cursor.execute("INSERT INTO {} VALUES {}".format(table_name, values)) 
 		self.conn.commit() #сохраняем изменения в БД
 
 	def show_table(self, table_name):
-		self.cursor.execute("SELECT * FROM {}".format(table_name)) #LIMIT 2')
+		''' Для показа всех данных прописывать имя таблицы строкой'''
+
+				self.cursor.execute("SELECT * FROM {}".format(table_name)) 
 		for row in self.cursor:
 			print(row)
 
 	def close(self):		
-
 		''' всегда закрывать каретку и БД'''
 
-		self.cursor.close() #закрываем каретку
+		self.cursor.close() #закрываем каретку/курсор
 		self.conn.close() #закрываем БД
 
+
 	def __str__(self):
-		return 'class Object postgree'
+		return 'class Object postgree DB'
+
+
+
 
 
 class open_xlsx:
 
-	def __init__(self):
-		self.wb = op.load_workbook(filename = 'd:/Users/A/Desktop/тест.xlsx')
-		self.sheets = self.wb.sheetnames
-		self.ws1 = self.wb[self.sheets[0]]
-		self.ws1_A_column = self.ws1['A']
+	def __init__(self, file_name, sheet_number):
+		''' Путь к файлу передавать строкой '''
+		''' Номер листа для копирования передавать целочисленным'''
+		self.wb = op.load_workbook(filename = file_name)
+		self.sheets = self.wb.sheetnames #загружаем список листов
+
+
+		self.ws = self.wb[self.sheets[sheet_number-1]] #передаем номер листа
+		''' Здесь обращение происходит по индексу, поэтому -> минус один'''
+
+		self.A_column = self.ws['A'] 
+		self.B_column = self.ws['B'] 
+		self.C_column = self.ws['C'] 
+		self.D_column = self.ws['D'] 
+		self.E_column = self.ws['E'] 
+		self.F_column = self.ws['F'] 
+		self.G_column = self.ws['G'] 
+		self.H_column = self.ws['H'] 
+		self.I_column = self.ws['I'] 
+		self.J_column = self.ws['J'] 
+		self.K_column = self.ws['K'] 
+		self.L_column = self.ws['L'] 
+		self.M_column = self.ws['M'] 
+		self.N_column = self.ws['N'] 
+		self.O_column = self.ws['O'] 
+		self.P_column = self.ws['P'] 
+		self.Q_column = self.ws['Q'] 
+		self.R_column = self.ws['R'] 
+		self.S_column = self.ws['S'] 
+		self.T_column = self.ws['T'] 
+		self.U_column = self.ws['U'] 
+		self.V_column = self.ws['V'] 
+		self.W_column = self.ws['W'] 
+		self.X_column = self.ws['X'] 
+		self.Y_column = self.ws['Y'] 
+		self.Z_column = self.ws['Z'] 
+		self.AA_column = self.ws['AA'] 
+		self.AB_column = self.ws['AB'] 
+		self.AC_column = self.ws['AC'] 
+		self.AD_column = self.ws['AD'] 
+		self.AE_column = self.ws['AE'] 
+		self.AF_column = self.ws['AF'] 
+		self.AG_column = self.ws['AG'] 
+
+
+	def take_the_values(self, name_column):
+		''' Имя колонки писать строкой, этот аргумент определяет конец считывания '''
 		for row in self.ws1_A_column[1:]:
 			print(row.value)
+		print(len(self.ws1_A_column))
+		#for 
 
 #a = postgree_connect('test_db')
 #a.show_table('test_table')
 #a.close()
 
-op_xl = open_xlsx()
+op_xl_1 = open_xlsx('d:/Users/A/Desktop/тест.xlsx', 1)
+#op_xl_1.take_the_values('G')
